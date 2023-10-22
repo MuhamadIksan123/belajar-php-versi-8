440d9d8d9a2e49b57a42fbb726465bb38679a6cc
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-10-23 05:46:28 +0700

    Belajar Fitur PHP Versi 8 Step 2

---
+++ FunctionOverridingClass.php
@@ -0,0 +1,14 @@
+<?php
+
+class ParentClass {
+    public function sampleFunction(string $name) {
+
+    }
+}
+
+class ChildFunction extends ParentClass {
+    public function sampleFunction(int $name)
+    {
+        
+    }
+}
---
+++ FunctionOverridingPrivate.php
@@ -0,0 +1,11 @@
+<?php
+
+class Manager {
+    private function test(string $data): void {}
+}
+
+class VicePresident extends Manager {
+    public function tes(int $data): string {
+        return "VP";
+    }
+}
